3fda6c87c4f4d596393d76f804006159e0d65931
danielb07 <danielbarraud07@gmail.com>
2020-04-22 19:24:53 +0530

    structure complete

--- sketch.js
+++ sketch.js
@@ -9,10 +9,12 @@ function setup() {
   tower3 = new Tower(340,250,90,200);
   tower4 = new Tower(430,225,70,175);
   tower5 = new Tower(500,200,70,300);
+  tower6 = new Tower(593,50,20,500);
+  tower7 = new Tower(160,50,20,500);
 
   // roof
   roof1 = new Roof(188,199,281,199,236,127) 
-  roof2 = new Roof(341,253,430,253,384,159) 
+  roof2 = new Roof(341,249,430,249,384,159) 
   roof3 = new Roof(493,199,577,199,533,127) 
 
   
@@ -25,6 +27,8 @@ function draw() {
   tower3.display();
   tower4.display();
   tower5.display();
+  tower6.display();
+  tower7.display();
 
   roof1.display();
   roof2.display();
